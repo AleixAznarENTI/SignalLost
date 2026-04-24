c365d4a9139a62dc6692571d61a1aff07833e73d
AleixAznar <aleixaznar9@gmail.com>
2026-04-24 03:25:50 +0200

    fix: organisation

--- Game.h
+++ Game.h
@@ -31,39 +31,42 @@
         sf::Font         m_font;
 
         // --- Systems ---
-        Map            m_map;
-        Player         m_player;
-        Camera         m_camera;
-        Renderer       m_renderer;
-        Flashlight     m_flashlight;
-        EnergySystem   m_energy;
-        HUD            m_hud;
-        AudioManager   m_audio;
-        ParticleSystem m_particles;
-        Minimap        m_minimap;
-        StatsTracker m_stats;
-        PostProcess m_postProcess;
-        PowerUpSystem m_powerUps;
-        sf::Color m_currentLightColor = sf::Color(255, 240, 200);
-        std::vector<Enemy> m_enemies;
-        bool m_enemyAlertPlayed = false;
+        Map              m_map;
+        Player           m_player;
+        Camera           m_camera;
+        Renderer         m_renderer;
+        Flashlight       m_flashlight;
+        EnergySystem     m_energy;
+        HUD              m_hud;
+        AudioManager     m_audio;
+        ParticleSystem   m_particles;
+        Minimap          m_minimap;
+        StatsTracker     m_stats;
+        PostProcess      m_postProcess;
+        PowerUpSystem    m_powerUps;
+        HazardZoneSystem m_hazards;
+
+        // --- Entities ---
+        std::vector<Battery> m_batteries;
+        std::vector<Enemy>   m_enemies;
+
+        // --- Game state ---
+        sf::Vector2f m_signalPos;
+        GameState    m_state;
+        GameState    m_prevState;
+        sf::Clock    m_clock;
+        HazardType   m_currentHazard = HazardType::None;
+        HazardType   m_prevHazard = HazardType::None;
+        sf::Color    m_currentLightColor = sf::Color(255, 240, 200);
+
+        // --- Timers ---
         float m_wakingTimer = 0.f;
         float m_winTimer = 0.f;
         float m_slowMotionTimer = 0.f;
         float m_gameOverFadeTimer = 0.f;
 
-        // --- Game state ---
-        std::vector<Battery> m_batteries;
-        sf::Vector2f         m_signalPos;
-        GameState            m_state;
-        GameState            m_prevState;
-        sf::Clock            m_clock;
-
-        // --- Hazard Zones ---
-        HazardZoneSystem m_hazards;
-        HazardType m_currentHazard = HazardType::None;
-        HazardType m_prevHazard = HazardType::None;
-
+        // --- Flags ---
+        bool m_enemyAlertPlayed = false;
 
         // --- The three phases of the game loop ---
         void processInput();
@@ -73,17 +76,19 @@
         // --- Helpers ---
         void initMap();
         void reset();
+        void spawnEnemies();
 
+        // --- Update subsystems ---
         void updateMovement(float dt);
         void updateRoomEffects(float dt, sf::Vector2i playerTile);
-        void updateBatteries(float dt);
         void updateHazardEffects(float dt, sf::Vector2i playerTile);
         void updateEnemies(float dt);
+        void updateBatteries(float dt);
         void updatePowerUps(float dt);
         void updateEnemyProximity();
         void checkEndConditions();
-        void spawnEnemies();
 
+        // --- Constants ---
         static constexpr int   MAP_W = 60;
         static constexpr int   MAP_H = 60;
         static constexpr float TILE_SIZE = 24.f;
